9dc8f5a83d4f8d54152bad37a47a4fe82f3828d1
Julian Zeilinger <>
2024-12-12 14:59:27 +0100

    updated pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 set -e
+
 dvc pull
-mlflow run . -e data_validation
-mlflow run . -e preprocessing
-mlflow run . -e feature_engineering
-mlflow run . -e hyperparameter_tuning
-mlflow run . -e training
-mlflow run . -e evaluation
+
+steps=("data_validation" "preprocessing" "feature_engineering" "hyperparameter_tuning" "training" "evaluation")
+failed_steps=()
+
+for step in "${steps[@]}"; do
+    echo "Running step: $step"
+    if ! mlflow run . -e "$step" --env-manager=local; then
+        echo "Step $step failed!"
+        failed_steps+=("$step")
+    fi
+done
+
+if [ ${#failed_steps[@]} -ne 0 ]; then
+    echo "The following steps failed: ${failed_steps[@]}"
+    exit 1
+else
+    echo "All steps completed successfully!"
+fi
